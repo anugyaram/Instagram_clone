52ed045e756adc6ad50dc4c86a261a8126ec6ab2
Anugya Ram <anugyaram08@gmail.com>
2023-03-24 20:34:56 +0530

    getting new data

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Header from '../../components/Header';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+
+    const [user,setProfile] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://dummyjson.com/users/1').then(res => {
+            setProfile(res.data);
+            console.log(res.data);
+        }).catch(error => (console.log(error)))
+    },[])
   return (
     <div>
-        <Header />
-        Home Page
+        <Header image={user.image} />
+        
     </div>
   )
 }
